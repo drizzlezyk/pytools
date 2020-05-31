54f5d1cebd4ae81ce61dd448f61d0bbab3b2ecc8
钟源珂 <zhongyuanke@zhongyuankedeMacBook-Pro.local>
2020-05-31 18:31:18 +0800

    modified:   network/vcca.py 	modified:   tests/vcca_test.py

--- network/vcca.py
+++ network/vcca.py
@@ -1,8 +1,5 @@
-import keras
-from keras.layers import Input, Dense, Activation, BatchNormalization, Dropout, Lambda
-from keras.regularizers import l1_l2
 from keras.models import Model, load_model
-from keras import backend
+from keras import backend as K
 from keras.losses import mse
 from keras import optimizers, regularizers, initializers
 from keras.layers import Input, Dense, Activation, BatchNormalization, Dropout, Lambda, Concatenate, concatenate
@@ -14,33 +11,36 @@ import tensorflow as tf
 
 def sampling(args):
     z_mean, z_log_var = args
-    batch = backend.shape(z_mean)[0]
-    dim = backend.int_shape(z_mean)[1]
-    epsilon = backend.random_normal(shape=(batch, dim))
-    return z_mean + backend.exp(0.5 * z_log_var) * epsilon
+    batch = K.shape(z_mean)[0]
+    dim = K.int_shape(z_mean)[1]
+    epsilon = K.random_normal(shape=(batch, dim))
+    return z_mean + K.exp(0.5 * z_log_var) * epsilon
 
 
-MeanAct = lambda x: tf.clip_by_value(backend.exp(x), 1e-5, 1e5)
+MeanAct = lambda x: tf.clip_by_value(K.exp(x), 1e-5, 1e5)
 
 
-class VCCA():
-    def __init__(self, input_size_x, input_size_y, validation_split=0, private=True, path='./vcca_result/', patience=60,):
-        self.encoder_hx = None
-        self.encoder_hy = None
-        self.encoder_z = None
-        self.decoder_x = None
-        self.decoder_y = None
-        self.vcca =None
-        self.path = '../vcca_result/'
+class VCCA:
+    def __init__(self, input_size_x,inputs_size_y, path="./",
+                 y_size=10,
+                 method="lognorm",
+                 validation_split=0.0,
+                 patience=60,
+                 deterministic=False):
+        self.input_size_x = input_size_x
+        self.input_size_y = inputs_size_y
+        self.vcca = None
+        self.inputs = None
+        self.outputs = None
+        self.path = path
+        self.initializers = "glorot_uniform"
+        self.method = method
         self.optimizer = optimizers.Adam(lr=0.01)
+        self.y_size = y_size
+        self.dropout_rate = 0.01
         self.kernel_regularizer = regularizers.l1_l2(l1=0.00, l2=0.00)
         self.validation_split = validation_split
-        self.input_size_x = input_size_x
-        self.input_size_y = input_size_y
-        self.private = private
-        self.inputs_y = 0
-        self.initializers = "glorot_uniform"
-
+        self.deterministic = deterministic
         callbacks = []
         checkpointer = ModelCheckpoint(filepath=path + "vae_weights.h5", verbose=1, save_best_only=False,
                                        save_weights_only=True)
@@ -54,149 +54,139 @@ class VCCA():
         self.callbacks = callbacks
 
     def build(self):
-        Relu = 'relu'
-        KL_hx_loss = 0
-        KL_hy_loss = 0
-        hx_z = tf.placeholder(tf.float32)
-        hy_z = tf.placeholder(tf.float32)
-        inputs_x = Input(shape=(self.input_size_x,), name="x")
+        # build encoder
 
-        if self.private:
-            self.inputs_y = Input(shape=(self.input_size_y,), name="y")
-            hx = Dense(128, kernel_regularizer=l1_l2(l1=0., l2=0.), name="encoder_hx1")(inputs_x)
-            hx = BatchNormalization(center=True, scale=False)(hx)
-            hx = Activation(Relu, name="activation_hx_1")(hx)
-            hx = Dense(64, kernel_regularizer=l1_l2(l1=0., l2=0.), name="encoder_hx2")(hx)
-            hx = BatchNormalization(center=True, scale=False)(hx)
-            hx = Activation(Relu, name="activation_hx_2")(hx)
-            hx_mean = Dense(32, kernel_regularizer=l1_l2(l1=0., l2=0.), name="mean_hx")(hx)
-            hx_var = Dense(32, kernel_regularizer=l1_l2(l1=0., l2=0.), name="var_hx")(hx)
-            hx_z = Lambda(sampling, output_shape=(32,), name='sample_hx_z')([hx_mean, hx_var])
-            self.encoder_hx = Model(inputs=[inputs_x], outputs=hx_z)
+        Relu = "relu"
+        inputs_x = Input(shape=(self.input_size_x,), name='inputs_x')
+        inputs_y = Input(shape=(self.input_size_y,), name='inputs_y')
 
-            hy = Dense(128, kernel_regularizer=l1_l2(l1=0., l2=0.), name="encoder_hy1")(self.inputs_y)
-            hy = BatchNormalization(center=True, scale=False)(hy)
-            hy = Activation(Relu, name="activation_hy_1")(hy)
-            hy = Dense(64, kernel_regularizer=l1_l2(l1=0., l2=0.), name="encoder_hy2")(hy)
-            hy = BatchNormalization(center=True, scale=False)(hy)
-            hy = Activation(Relu, name="activation_hy_2")(hy)
-            hy_mean = Dense(32, kernel_regularizer=l1_l2(l1=0., l2=0.), name="center_hy_mean")(hy)
-            hy_var = Dense(32, kernel_regularizer=l1_l2(l1=0., l2=0.), name="center_hy_var")(hy)
-            hy_z = Lambda(sampling, output_shape=(32,), name='sample_hy_z')([hy_mean, hy_var])
-            self.encoder_hy = Model(inputs=[self.inputs_y], outputs=hy_z)
+        hx = Dense(128, kernel_regularizer=self.kernel_regularizer, kernel_initializer=self.initializers,
+                  bias_initializer='zeros', name='hx_hidden_layer_x1')(inputs_x)
+        hx = BatchNormalization(center=True, scale=False)(hx)
+        hx = Activation(Relu)(hx)
+        hx = Dropout(self.dropout_rate)(hx)
 
-            KL_hx_loss = -0.5 * backend.sum(1 + hx_var - backend.square(hx_mean) - backend.exp(hx_var), axis=-1)
-            KL_hy_loss = -0.5 * backend.sum(1 + hy_var - backend.square(hy_mean) - backend.exp(hy_var), axis=-1)
+        hx = Dense(64, kernel_regularizer=self.kernel_regularizer, kernel_initializer=self.initializers,
+                  name='hx_hidden_layer_x2')(hx)
+        hx = BatchNormalization(center=True, scale=False)(hx)
+        hx = Activation(Relu)(hx)
+        hx = Dropout(self.dropout_rate)(hx)
 
-        z1 = Dense(128, kernel_regularizer=l1_l2(l1=0., l2=0.), name="encoder_z_mean_1")(inputs_x)
-        z1 = BatchNormalization(center=True, scale=False)(z1)
-        z1 = Activation(Relu, name="activation_z_mean_1")(z1)
-        z1 = Dense(64, kernel_regularizer=l1_l2(l1=0., l2=0.), name="encoder_z_mean_2")(z1)
-        z1 = BatchNormalization(center=True, scale=False)(z1)
-        z1 = Activation(Relu, name="activation_z_mean_2")(z1)
-        z_mean = Dense(32, kernel_regularizer=l1_l2(l1=0., l2=0.), name="center_z_mean")(z1)
+        hx_mean = Dense(32, kernel_regularizer=self.kernel_regularizer, kernel_initializer=self.initializers,
+                       name="hx_mean")(hx)
+        hx_log_var = Dense(32, kernel_regularizer=self.kernel_regularizer, kernel_initializer=self.initializers,
+                          name="hx_log_var")(hx)
+        hx_z = Lambda(sampling, output_shape=(32,), name='hx_z')([hx_mean, hx_log_var])
+        encoder_hx = Model(inputs_x, [hx_mean, hx_log_var, hx_z], name='encoder_hx')
 
-        z2 = Dense(128, kernel_regularizer=l1_l2(l1=0., l2=0.), name="encoder_z_var_1")(inputs_x)
-        z2 = BatchNormalization(center=True, scale=False)(z2)
-        z2 = Activation(Relu, name="activation_z_var_1")(z2)
-        z2 = Dense(64, kernel_regularizer=l1_l2(l1=0., l2=0.), name="encoder_z_var_2")(z2)
-        z2 = BatchNormalization(center=True, scale=False)(z2)
-        z2 = Activation(Relu, name="activation_z_var_2")(z2)
-        z_var = Dense(32, kernel_regularizer=l1_l2(l1=0., l2=0.), name="center_z_var")(z2)
-        z = Lambda(sampling, output_shape=(32,), name='hidden_var_z')([z_mean, z_var])
-        print(z)
-        self.encoder_z = Model(inputs=inputs_x, outputs=z_mean)
+        hy = Dense(128, kernel_regularizer=self.kernel_regularizer, kernel_initializer=self.initializers,
+                   bias_initializer='zeros', name='hy_hidden_layer_x1')(inputs_y)
+        hy = BatchNormalization(center=True, scale=False)(hy)
+        hy = Activation(Relu)(hy)
+        hy = Dropout(self.dropout_rate)(hy)
 
-        latent_inputs_z = Input(shape=(32,), name='latent_input_z')
-        if self.private:
+        hy = Dense(64, kernel_regularizer=self.kernel_regularizer, kernel_initializer=self.initializers,
+                   name='hy_hidden_layer_x2')(hy)
+        hy = BatchNormalization(center=True, scale=False)(hy)
+        hy = Activation(Relu)(hy)
+        hy = Dropout(self.dropout_rate)(hy)
 
-            latent_inputs_x = Input(shape=(32,), name='latent_inputs_x')
-            latent_inputs_y = Input(shape=(32,), name='latent_inputs_y')
-            input_decoder_x = concatenate([latent_inputs_x, latent_inputs_z])
-            input_decoder_y = concatenate([latent_inputs_y, latent_inputs_z])
-            x = Dense(64, kernel_regularizer=l1_l2(l1=0., l2=0.), kernel_initializer=self.initializers, name="decoder_x_1")(input_decoder_x)
-            x = BatchNormalization(center=True, scale=False)(x)
-            x = Activation(Relu, name="activation_x_1")(x)
-            x = Dense(128, kernel_regularizer=l1_l2(l1=0., l2=0.), name="decoder_x_2")(x)
-            x = BatchNormalization(center=True, scale=False)(x)
-            x = Activation(Relu, name="activation_x_2")(x)
-            output_x = Dense(self.input_size_x, kernel_regularizer=l1_l2(l1=0., l2=0.), name="decoder_x_3")(x)
-            self.decoder_x = Model(inputs=[latent_inputs_x, latent_inputs_z], outputs=output_x)
+        hy_mean = Dense(32, kernel_regularizer=self.kernel_regularizer, kernel_initializer=self.initializers,
+                        name="hy_mean")(hy)
+        hy_log_var = Dense(32, kernel_regularizer=self.kernel_regularizer, kernel_initializer=self.initializers,
+                           name="hy_log_var")(hy)
+        hy_z = Lambda(sampling, output_shape=(32,), name='hy_z')([hy_mean, hy_log_var])
 
-            y = Dense(64, kernel_regularizer=l1_l2(l1=0., l2=0.), name="decoder_y_1")(input_decoder_y)
-            y = BatchNormalization(center=True, scale=False)(y)
-            y = Activation(Relu, name="activation_y_1")(y)
-            y = Dense(128, kernel_regularizer=l1_l2(l1=0., l2=0.), name="decoder_y_2")(y)
-            y = BatchNormalization(center=True, scale=False)(y)
-            y = Activation(Relu, name="activation_y_2")(y)
-            output_y = Dense(self.input_size_y, kernel_regularizer=l1_l2(l1=0., l2=0.), name="decoder_y_3")(y)
-            self.decoder_y = Model(inputs=[latent_inputs_y, latent_inputs_z], outputs=output_y)
+        z = Dense(128, kernel_regularizer=self.kernel_regularizer, kernel_initializer=self.initializers,
+                   bias_initializer='zeros', name='z_hidden_layer_x1')(inputs_x)
+        z = BatchNormalization(center=True, scale=False)(z)
+        z = Activation(Relu)(z)
+        z = Dropout(self.dropout_rate)(z)
 
-        else:
-            x = Dense(64, kernel_regularizer=l1_l2(l1=0., l2=0.), kernel_initializer=self.initializers, name="decoder_x_1")(latent_inputs_z)
-            x = BatchNormalization(center=True, scale=False)(x)
-            x = Activation(Relu, name="activation_x_1")(x)
-            x = Dense(128, kernel_regularizer=l1_l2(l1=0., l2=0.), name="decoder_x_2")(x)
-            x = BatchNormalization(center=True, scale=False)(x)
-            x = Activation(Relu, name="activation_x_2")(x)
-            output_x = Dense(self.input_size_x, kernel_regularizer=self.kernel_regularizer,
-                            kernel_initializer=self.initializers, activation="linear")(x)
-            self.decoder_x = Model(latent_inputs_z, output_x)
+        z = Dense(64, kernel_regularizer=self.kernel_regularizer, kernel_initializer=self.initializers,
+                   name='z_hidden_layer_x2')(z)
+        z = BatchNormalization(center=True, scale=False)(z)
+        z = Activation(Relu)(z)
+        z = Dropout(self.dropout_rate)(z)
 
-            y = Dense(64, kernel_regularizer=l1_l2(l1=0., l2=0.), name="decoder_y_1")(latent_inputs_z)
-            y = BatchNormalization(center=True, scale=False)(y)
-            y = Activation(Relu, name="activation_y_1")(y)
-            y = Dense(128, kernel_regularizer=l1_l2(l1=0., l2=0.), name="decoder_y_2")(y)
-            y = BatchNormalization(center=True, scale=False)(y)
-            y = Activation(Relu, name="activation_y_2")(y)
-            output_y = Dense(self.input_size_y, kernel_regularizer=l1_l2(l1=0., l2=0.), name="decoder_y_3")(y)
-            self.decoder_y = Model(inputs=latent_inputs_z, outputs=output_y)
+        z_mean = Dense(32, kernel_regularizer=self.kernel_regularizer, kernel_initializer=self.initializers,
+                        name="z_mean")(z)
+        z_log_var = Dense(32, kernel_regularizer=self.kernel_regularizer, kernel_initializer=self.initializers,
+                           name="z_log_var")(z)
+        z = Lambda(sampling, output_shape=(32,), name='z')([hx_mean, hx_log_var])
+        encoder_z = Model(inputs_x, [z_mean, z_log_var, z], name='encoder_z')
 
-        reconstruction_loss1 = mse(inputs_x, output_x)
-        reconstruction_loss2 = mse(self.inputs_y, output_y)
 
-        KLz_loss = -0.5 * backend.sum(1 + z_var - backend.square(z_mean) - backend.exp(z_var), axis=-1)
-        if self.private:
-            loss = KL_hx_loss+KL_hy_loss+KLz_loss+reconstruction_loss1+reconstruction_loss2
-        else:
-            loss = KLz_loss+reconstruction_loss1+reconstruction_loss2
-        if self.private:
+        # x = Dense(32, kernel_regularizer=self.kernel_regularizer, kernel_initializer=self.initializers)(latent_inputs)
+        # x = BatchNormalization(center=True, scale=False)(x)
+        # x = Activation(Relu)(x)
+        latent_x = concatenate([hx_z, z])
+        x = Dense(64, kernel_regularizer=self.kernel_regularizer, kernel_initializer=self.initializers)(latent_x)
+        x = BatchNormalization(center=True, scale=False)(x)
+        x = Activation(Relu)(x)
+        x = Dropout(self.dropout_rate)(x)
+        x = Dense(128, kernel_regularizer=self.kernel_regularizer, kernel_initializer=self.initializers)(x)
+        x = BatchNormalization(center=True, scale=False)(x)
+        x = Activation(Relu)(x)
+        x = Dropout(self.dropout_rate)(x)
+        outputs_x = Dense(self.input_size_x, kernel_regularizer=self.kernel_regularizer,
+                        kernel_initializer=self.initializers, activation="linear")(x)
 
-            output1 = self.decoder_x([self.encoder_hx(inputs_x), self.encoder_z(inputs_x)])
-            output2 = self.decoder_y([self.encoder_hy(self.inputs_y), self.encoder_z(inputs_x)])
-            vcca = Model([inputs_x, self.inputs_y], [output1, output2])
-            vcca.add_loss(loss)
-            self.vcca = vcca
-        else:
-            output1 = self.decoder_x(self.encoder_z(inputs_x))
-            output2 = self.decoder_y(self.encoder_z(inputs_x))
-            vcca = Model(inputs_x, [output1, output2])
-            vcca.add_loss(loss)
-            self.vcca = vcca
+        latent_y = concatenate([hy_z, z])
+        y = Dense(64, kernel_regularizer=self.kernel_regularizer, kernel_initializer=self.initializers)(latent_y)
+        y = BatchNormalization(center=True, scale=False)(y)
+        y = Activation(Relu)(y)
+        y = Dropout(self.dropout_rate)(y)
+        y = Dense(128, kernel_regularizer=self.kernel_regularizer, kernel_initializer=self.initializers)(y)
+        y = BatchNormalization(center=True, scale=False)(y)
+        y = Activation(Relu)(y)
+        y = Dropout(self.dropout_rate)(y)
+        outputs_y = Dense(self.input_size_y, kernel_regularizer=self.kernel_regularizer,
+                            kernel_initializer=self.initializers, activation="linear", name='outputs_y')(y)
+        # decoder_y = Model(latent_inputs, outputs_y, name='decoder_y')
+        # decoder_x = Model(latent_inputs, outputs_x, name='decoder_x')
+
+        # outputs_x = decoder_x(encoder(inputs_x)[0])
+        # outputs_y = decoder_y(encoder(inputs_x)[0])
+
+        vcca = Model([inputs_x, inputs_y], [outputs_x, outputs_y], name='vae_mlp')
+        reconstruction_loss_x = mse(inputs_x, outputs_x)
+        reconstruction_loss_x *= self.input_size_x
+        reconstruction_loss_y = mse(inputs_y, outputs_y)
+        reconstruction_loss_y *= self.input_size_y
+        kl_loss_z = 1 + z_log_var - K.square(z_mean) - K.exp(z_log_var)
+        kl_loss_z = -0.5 * K.sum(kl_loss_z, axis=-1)
+        kl_loss_hx = 1 + hx_log_var - K.square(hx_mean) - K.exp(hx_log_var)
+        kl_loss_hx = -0.5 * K.sum(kl_loss_hx, axis=-1)
+        kl_loss_hy = 1 + hy_log_var - K.square(hy_mean) - K.exp(hy_log_var)
+        kl_loss_hy = -0.5 * K.sum(kl_loss_hy, axis=-1)
+
+        vae_loss = reconstruction_loss_x + reconstruction_loss_y + kl_loss_z + kl_loss_hx + kl_loss_hy
+
+        vcca.add_loss(vae_loss)
+        self.vcca = vcca
+        self.encoder_z = encoder_z
+        # self.decoder_x = decoder_x
 
     def compile(self):
         self.vcca.compile(optimizer=self.optimizer)
         self.vcca.summary()
 
-    def train(self, x, y, batch_size=256, epochs=300):
-        if os.path.isfile(self.path + "vcca_weights.h5"):
-            self.vcca.load_weights(self.path + "vcca_weights.h5")
-        else:
-            if self.private:
-                self.vcca.fit([x, y], epochs=epochs, batch_size=batch_size, callbacks=self.callbacks,
-                              validation_split=self.validation_split, shuffle=True)
-            else:
+    def train(self, x, y, batch_size=1, epochs=300):
+        # if os.path.isfile(self.path + "vae_weights.h5"):
+        #     self.vae.load_weights(self.path + "vae_weights.h5")
+        # else:
+        #
+        self.vcca.fit({'inputs_x': x, 'inputs_y': y}, epochs=epochs, batch_size=batch_size,
+                         validation_split=self.validation_split, shuffle=True)
+            # self.vae.model.save(self.path + "/model_best.h5")
 
-                self.vcca.fit(x, epochs=epochs, batch_size=batch_size, callbacks=self.callbacks,
-                              validation_split=self.validation_split, shuffle=True)
-
-    def integrate(self, xadata, yadata, out_path, save=False):
-        [z_sample] = self.encoder_z.predict(xadata.X)
-        xadata.obsm['mid'] = z_sample
-        print(z_sample.shape)
-        if save:
-            xadata.write(out_path)
+    def integrate(self, xadata, save=True, use_mean=True):
+        [z_mean, z_log_var, z] = self.encoder_z.predict(xadata.X)
+        xadata.obsm['mid'] = z_mean
         return xadata
+
+
 
 
 
--- tests/vcca_test.py
+++ tests/vcca_test.py
@@ -18,29 +18,41 @@ label_path = '293t_jurkat_cluster.txt'
 batch_path = 'merge_result/merge5.csv'
 out_path = 'vcca/293t_vcca.h5ad'
 # file = 'merge_result/293t_jurkat.h5ad'
+file = 'scvi/scvi_batch_example_no_filt.h5ad'
 file_x = 'pbmc/293t/hg19'
 file_y = 'pbmc/293t_jurkat_50_50/hg19'
+adata = pp.read_sc_data(base_path+file)
 adata_x = pp.read_sc_data(base_path+file_x, fmt='10x_mtx')
-adata_y = pp.read_sc_data(base_path+file_x, fmt='10x_mtx')
+adata_y = pp.read_sc_data(base_path+file_y, fmt='10x_mtx')
 sc.pp.filter_genes(adata_x, min_cells=500)
 sc.pp.filter_genes(adata_y, min_cells=500)
-net = VCCA(input_size_x=adata_x.shape[1], input_size_y=adata_y.shape[1], path=result_path, private=True)
+#
 x = adata_x.X.toarray()
 y = adata_y.X.toarray()
+x = x[0:1000, ]
+y = y[0:1000, ]
+y = np.array(y, dtype='float32')
+print(y.shape)
+# print(x)
+# print(y)
+# print(adata.shape)
+# sc.pp.filter_genes(adata, min_cells=500)
+# sc.pp.filter_cells(adata, min_genes=1000)
+# print(adata.shape)
 
-net.inputs_y = tf.convert_to_tensor(y)
+net = VCCA(input_size_x=adata_x.shape[1], inputs_size_y=adata_y.shape[1], path=result_path)
+# net.inputs_y = tf.convert_to_tensor(y)
 # net = VAE(input_size=adata_all.shape[1], path=result_path, patience=patience)
 net.build()
 net.compile()
 
-print(x.shape)
-print(y.shape)
-net.train(x, y, epochs=epochs)
+
+net.train(x, y, epochs=epochs, batch_size=200)
 
 # net.encoder.load_weights(result_path+"vae_weights.h5")
 # net.decoder.load_weights(result_path+"decoder_weights.h5")
-yadata = net.integrate(adata_x, adata_y, out_path)
-
+yadata = net.integrate(adata_x, out_path)
+print(yadata)
 # x_tsne = TSNE(n_components=2).fit_transform(yadata.obsm['X_scxx'])
 
 # plot.plot_scatter(x_tsne, base_path+label_path, result_path+'mid07.png', 'batch')
